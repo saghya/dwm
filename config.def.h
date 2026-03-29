4d21e4052a34a5c4a2fe31812ab51784a9eeb9c7
saghya <saghyattila77@gmail.com>
2026-03-29 19:04:09 +0200

    rename dmenu scripts

--- config.def.h
+++ config.def.h
@@ -72,12 +72,12 @@ static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]      = { TERMINAL, NULL };
 static const char *browser[]      = { "firefox", NULL };
 static const char *file_manager[] = { TERMINAL, "--class", "ranger", "-e", "ranger", NULL };
-static const char *powermenu[]    = { "powermenu", "-m", dmenumon, NULL };
+static const char *powermenu[]    = { "dmenu-powermenu", "-m", dmenumon, NULL };
 static const char *network[]      = { "networkmanager_dmenu", "-m", dmenumon, "-l", "20", NULL };
 static const char *bluetooth[]    = { "dmenu-bluetooth", "-m", dmenumon, NULL };
 static const char *passmenu[]     = { "passmenu", "-m", dmenumon, NULL };
 static const char *screenshot[]   = { "dmenu-screenshot", "-m", dmenumon, NULL };
-static const char *todo[]         = { "todo", "-m", dmenumon, NULL };
+static const char *todo[]         = { "dmenu-todo", "-m", dmenumon, NULL };
 static const char *vpn[]          = { "dmenu-vpn", "-m", dmenumon, NULL };
 static const char *xrandr[]       = { "dmenu-xrandr", "-m", dmenumon, NULL };
 
